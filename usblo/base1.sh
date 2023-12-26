57a647a627dea62109e51abc3f4800b084cb16ed
root <root@pve1.fritz.box>
2023-12-27 00:16:39 +0100

    nextlevel

--- usblo/base1.sh
+++ usblo/base1.sh
@@ -12,22 +12,24 @@ read -p "Enter the line number to retrieve the UUID: " line_number
 
 # Retrieve UUID based on the chosen line number
 chosen_line=$(echo "$blkid_output" | sed -n "${line_number}p")
-TARGET_UUID=$(echo "$chosen_line" | grep -o 'UUID="[^"]*' | sed 's/UUID="//')
+
+# Extract UUID from the chosen line
+TARGET_UUID=$(echo "$chosen_line" | grep -oP ' UUID="\K[^"]*')
 
 echo "The selected UUID is: $TARGET_UUID"
 
-# Get information about the UUID entry in blkid
-device=$(echo "$chosen_line" | awk -F':' '{print $1}')
-uuid_entry=$(echo "$chosen_line" | awk -F' ' '{print $2}')
-
-# Check if the device is already present in /etc/fstab
-if grep -q "$TARGET_UUID" /etc/fstab; then
+# Check if the device's UUID is already present in /etc/fstab
+if grep -q "UUID=$TARGET_UUID" /etc/fstab; then
     echo "This UUID is already present in /etc/fstab."
 else
-    # Backup /etc/fstab
-    cp /etc/fstab /etc/fstab.backup
-
+    # Check if the script is run as root
+    if [ "$EUID" -ne 0 ]; then
+        echo "Please run this script as root to modify /etc/fstab."
+        exit 1
+    fi
+    
     # Append entry to /etc/fstab for auto-mounting
-    echo "UUID=$TARGET_UUID  /mnt/local-usb  auto  defaults  0  2" >> /etc/fstab
+    echo "UUID=$TARGET_UUID /mnt/local-usb auto defaults 0 2" >> /etc/fstab
     echo "Entry added to /etc/fstab for auto-mounting."
 fi
+
