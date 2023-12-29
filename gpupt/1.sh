ef9f95202573d72d8e66e12f22d812894a61bbf2
maxwagne <126621706+maxwagne@users.noreply.github.com>
2023-12-29 01:45:59 +0100

    Update 1.sh

--- gpupt/1.sh
+++ gpupt/1.sh
@@ -8,6 +8,10 @@ sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet i
 update-grub
 update-grub2
 
-# Reboot the system
-reboot now
-
+# Perform system reboot
+read -p "Do you want to reboot the system now? (y/n): " REBOOT_CONFIRM
+if [ "$REBOOT_CONFIRM" = "y" ]; then
+    reboot
+else
+    echo "System reboot was not executed. Please manually restart the system to apply the changes."
+fi
