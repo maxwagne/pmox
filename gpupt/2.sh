253f50c0b73d80e485608a9c1d9fa619e1efa030
maxwagne <126621706+maxwagne@users.noreply.github.com>
2023-12-29 01:46:22 +0100

    Update 2.sh

--- gpupt/2.sh
+++ gpupt/2.sh
@@ -8,6 +8,10 @@ echo "vfio_pci" >> /etc/modules
 # Update initramfs
 update-initramfs -u -k all
 
-# Reboot again this time with systemctl
-systemctl reboot
-
+# Perform system reboot
+read -p "Do you want to reboot the system now? (y/n): " REBOOT_CONFIRM
+if [ "$REBOOT_CONFIRM" = "y" ]; then
+    reboot
+else
+    echo "System reboot was not executed. Please manually restart the system to apply the changes."
+fi
